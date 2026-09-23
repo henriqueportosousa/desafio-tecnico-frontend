432620b3a1e46b6659d2fd23082eb27e41e3155c
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-09-23 10:33:12 -0300

    Final

--- src/pages/ListarChamados.jsx
+++ src/pages/ListarChamados.jsx
@@ -17,22 +17,28 @@ function ListarChamado({ chamados, aoExcluir }) {
             <h1>Lista de chamados</h1>
             <span style={{ fontWeight: "bold", color: "black" }}>Desafio Tecnico. Desenvolvindo por Henrique Porto de Sousa</span>
             <ul className="lista">
-                {chamados.map((chamado) => (
-                    <li key={chamado.id}>
-                        <strong>Titulo: {chamado.titulo}</strong>
-                        <span>ID: {chamado.id}</span>
-                        <span>Desricao: {chamado.descricao}</span>
-                        <span>Priopriedade: {chamado.prioridade}</span>
-                        <span>Solicitante: {chamado.solicitante}</span>
-                        <span>Status: {chamado.status}</span>
-
-                        <div className="acoes">
-                            <Link to={`/chamados/editar/${chamado.id}`} className="botao-alterar">Alterar</Link>
-                            <button onClick={() => confirmarExclusao(chamado)} className="botao-excluir">Excluir</button>
-                        </div>
-
-                    </li>
-                ))}
+                {chamados?.length === 0 ? (
+                    <li className="item-vazio">Nenhum chamado cadastrado.</li>
+                ) : (
+                    chamados.map((chamado) => (
+                        <li key={chamado.id}>
+                            <strong>Título: {chamado.titulo}</strong>
+                            <span>ID: {chamado.id}</span>
+                            <span>Descrição: {chamado.descricao}</span>
+                            <span>Prioridade: {chamado.prioridade}</span>
+                            <span>Solicitante: {chamado.solicitante}</span>
+                            <span>Status: {chamado.status}</span>
+                            <div className="acoes">
+                                <Link to={`/chamados/editar/${chamado.id}`} className="botao-alterar">
+                                    Alterar
+                                </Link>
+                                <button onClick={() => confirmarExclusao(chamado)} className="botao-excluir">
+                                    Excluir
+                                </button>
+                            </div>
+                        </li>
+                    ))
+                )}
             </ul>
 
             <Link to="/chamados">Voltar para Gerenciamento de
