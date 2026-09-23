44fd7942def308d98ccbc8ccfb3f3240fe0ad665
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-09-23 08:46:17 -0300

    Save 3

---
+++ src/pages/Chamados.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+
+function Chamados() {
+    return (
+        <div className="pagina">
+            <h1>Gerenciamento de Chamados</h1>
+            <p>Escolha uma das opções:</p>
+            <div className="opcoes">
+                <Link to="/chamados/listar">
+                    Listar Chamados
+                </Link>
+                <Link to="/chamados/cadastrar">
+                    Cadastrar novo chamado
+                </Link>
+            </div>
+            <Link to="/">
+                Voltar para a página inicial
+            </Link>
+        </div>
+    )
+}
+
+export default Chamados
