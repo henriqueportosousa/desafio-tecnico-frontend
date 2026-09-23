36be03b40bad35287ab930925c28c5eadc0fddac
henriqueportosousa <henrique.sousa4@aluno.cps.sp.gov.br>
2026-09-23 08:44:52 -0300

    Save 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,129 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
+import { useState } from 'react'
+import { Routes, Route } from 'react-router';
+
+import Cabecalho from "./components/Cabecalho";
+import CardModulo from "./components/CardModulo";
+
+import CadastroChamado from './pages/CadastroChamado';
+
+import chamadosInicias from './data/chamados';
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [mostrarModulos, setMostrarModulos] = useState(true);
+  const [chamados, setChamados] = useState(chamadosInicias);
+
+  const [modulos] = useState([
+    {
+      id: 1,
+      titulo: "Gerenciamento de Chamados",
+      descricao: "Cadastrar, consulte, excluir, alterar os chamados disponíveis.",
+      rota: '/chamados'
+    },
+  ]);
+
+
+  function adicionarChamado(novoChamado) {
+    const chamadoComId = {
+      id: Date.now(),
+      ...novoChamado,
+    }
+    setChamados((listaAtual) => [
+      ...listaAtual,
+      chamadoComId,
+    ]);
+
+    function exluirChamado(id) {
+      setChamados((listaAtual) =>
+        listaAtual.filter((chamado) => chamado.id !== id)
+      );
+    }
+
+    function alterarChamado(chamadoAtualizado) {
+      setChamados((listaAtual) =>
+        listaAtual.map((chamado) =>
+          chamado.id === chamadoAtualizado.id
+            ? chamadoAtualizado
+            : chamado
+        )
+      );
+    }
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="aplicacao">
+            <Cabecalho />
+            <main className="conteudo-principal">
 
-      <div className="ticks"></div>
+              <p className="introducao">
+                Desafio Tecnico. Desenvolvindo Por Henrique Porto de Sousa
+              </p>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+              <button
+                type="button"
+                className="botao-alternar"
+                onClick={() => setMostrarModulos(!mostrarModulos)}
+              >
+                {mostrarModulos ? "Ocultar módulos" : "Exibir módulos"}
+              </button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+
+              {mostrarModulos && (
+                <section className="modulos">
+                  {modulos.map((modulo) => (
+                    <CardModulo
+                      key={modulo.id}
+                      titulo={modulo.titulo}
+                      descricao={modulo.descricao}
+                      rota={modulo.rota}
+                    />
+                  ))}
+                </section>
+              )}
+            </main>
+          </div>
+        }
+      />
+
+      <Route
+        path='/chamados'
+        element={
+          <Chamados />
+        }
+      />
+
+      <Route
+        path='/chamados/listar'
+        element={
+          <Chamados
+            
+          />
+        }
+      />
+
+      <Route
+        path='/chamados/cadastrar'
+        element={
+          <CadastroChamado
+            chamados={chamados}
+            aoCadastrar={adicionarChamado}
+          />
+        }
+      />
+
+      <Route
+        path='/chamados/editar/:id'
+        element={
+          <Chamados />
+        }
+      />
+    </Routes>
   )
 }
 
